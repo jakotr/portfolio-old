4f384ac5ec7d67904d893a64b407d8b653624a1b
Jakub Otreba <jakub.otreba1@gmail.com>
2020-06-30 10:49:41 +0200

    update navbar for rwd

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 $('.header__menu-ham').on('click', function() {
     $('i').toggleClass('show');
-    $('.header__list').toggleClass('come');
+    $('.header__list__ham').toggleClass('come');
 });
 
 $('a').on('click', function(event) {
@@ -8,6 +8,8 @@ $('a').on('click', function(event) {
         $('body, html').animate({
             scrollTop: $(`.${$(event.target).data('scroll')}`).offset().top - 100
         }, 1000);
+        $('.header__list__ham').toggleClass('come');
+        $('i').toggleClass('show');
     }
 });
 
